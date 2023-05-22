3316ca5468b363765ce6fa49854a373058b45793
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-05-22 15:22:55 +0530

    Update Wrapper.java

--- src/main/java/tech/altier/Wrapper.java
+++ src/main/java/tech/altier/Wrapper.java
@@ -1,4 +1,24 @@
 package tech.altier;
 
+import com.whatsapp.api.WhatsappApiFactory;
+import com.whatsapp.api.domain.messages.Message;
+import com.whatsapp.api.domain.messages.TextMessage;
+import com.whatsapp.api.impl.WhatsappBusinessCloudApi;
+import com.whatsapp.api.utils.Formatter;
+
 public class Wrapper {
+    public static void main(String[] args) {
+        WhatsappApiFactory factory = WhatsappApiFactory.newInstance("a95fb9d5cd12a501c157aea016414ee5");
+
+        WhatsappBusinessCloudApi whatsappBusinessCloudApi = factory.newBusinessCloudApi();
+
+        var message = Message.MessageBuilder.builder()//
+                .setTo()//
+                .buildTextMessage(new TextMessage()//
+                        .setBody(Formatter.bold("Hello world!") + "\nSome code here: \n" + Formatter.code("hello world code here"))//
+                        .setPreviewUrl(false));
+
+
+        whatsappBusinessCloudApi.sendMessage(PHONE_NUMBER_ID, message);
+    }
 }
