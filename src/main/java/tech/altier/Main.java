57d878108e5c49bc67652fda682d4fb2392ae43c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-05-22 14:48:34 +0530

    Update Main.java

--- src/main/java/tech/altier/Main.java
+++ src/main/java/tech/altier/Main.java
@@ -1,53 +1,29 @@
 package tech.altier;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class Main {
-    private static String API_KEY = "e4e79003d371d80a3fb7f7be39e1f6ee";
+    public static void main( String[] args ) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://graph.facebook.com/v13.0/0788978085/messages"))
+                    .header("Authorization", "Bearer <YOUR BEARER TOKEN>")
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString("{ \"messaging_product\": \"whatsapp\", \"recipient_type\": \"individual\", \"to\": \"<TARGET PHONE NUMBER>\", \"type\": \"template\", \"template\": { \"name\": \"hello_world\", \"language\": { \"code\": \"en_US\" } } }"))
+                    .build();
+            HttpClient http = HttpClient.newHttpClient();
+            HttpResponse<String> response = http.send(request,BodyHandlers.ofString());
+            System.out.println(response.body());
 
-    public static void main(String[] args) throws JSONException, IOException {
-        chat("hello_world");
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-
-/**
-* ChatGPT API caller function
-* This method will take a string as input and return a string as output
-* @return response
-*/
-public static String chat(String input) throws IOException, JSONException {
-    String URL = "https://graph.facebook.com/v16.0/1384765148735827/messages";
-    HttpURLConnection con = (HttpURLConnection) new URL(URL).openConnection();
-
-    con.setRequestMethod("POST");
-    con.setRequestProperty("Content-Type", "application/json");
-    con.setRequestProperty("Authorization", "Bearer " + API_KEY);
-
-    JSONObject data = new JSONObject();
-    data.put("messaging_product", "whatsapp");
-    data.put("to", "+94761824607");
-    data.put("type", "template");
-    JSONObject template = new JSONObject();
-    template.put("name", input);
-    JSONObject language = new JSONObject();
-    language.put("code", "en_US");
-    template.put("language", language);
-    data.put("template", template);
-
-    con.setDoOutput(true);
-    con.getOutputStream().write(data.toString().getBytes());
-
-    String output = new BufferedReader(new InputStreamReader(con.getInputStream()))
-            .lines()
-            .reduce((a, b) -> a + b)
-            .get();
-
-    return new JSONObject(output).getJSONArray("choices").getJSONObject(0).getString("text");
-}
 }
