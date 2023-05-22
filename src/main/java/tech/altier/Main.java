8c686e709c843f9c685f9652fec0023d2c4c73f8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-05-22 15:09:25 +0530

    Update Main.java

--- src/main/java/tech/altier/Main.java
+++ src/main/java/tech/altier/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main( String[] args ) {
         try {
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI("https://graph.facebook.com/v13.0/a95fb9d5cd12a501c157aea016414ee5/messages"))
+                    .uri(new URI("https://graph.facebook.com/v13.0//messages"))
                     .header("Authorization", "Bearer EAATrb8LJqVMBAMNvZADo5quwhSXX61T531fyUZBWKHJYmNLloMTlFtGEshzEqYv6OLKAoP6ZAvRWdRZBfGaqJZBFF1ue6xknwGiYoVeGTZCTW49ypjXcl07bC6FIsc1iMcCF6n6PNz16gc0V7PdCww1ooz23x0fZAKZBZAVC1ch0jFsScPlZA1EXTlSXmA8V7MZAZBRyRQUz0Trocxfk8ho2GiXb4norlb99cJ0ZD")
                     .header("Content-Type", "application/json")
                     .POST(HttpRequest.BodyPublishers.ofString("{ \"messaging_product\": \"whatsapp\", \"recipient_type\": \"individual\", \"to\": \"+94786241686\", \"type\": \"template\", \"template\": { \"name\": \"hello_world\", \"language\": { \"code\": \"en_US\" } } }"))
